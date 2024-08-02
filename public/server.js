485c48c97166a89d0b954a0ba1b515fee4d6a459
jaaferna <jaaferna@bancolombia.com.co>
2024-08-02 15:06:12 -0500

    Ajuste código server

--- public/server.js
+++ public/server.js
@@ -8,6 +8,18 @@ const libxmljs = require('libxmljs2');
 const app = express(); // Crear una aplicación Express
 const PORT = 3000; // Definir el puerto en el que el servidor escuchará
 
+// Configurar multer para almacenar archivos en la carpeta "upload"
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) { // Función para definir la carpeta de destino
+        cb(null, 'upload/'); // Guardar archivos en la carpeta "upload"
+    },
+    filename: function (req, file, cb) { // Función para definir el nombre del archivo
+        cb(null, file.originalname); // Usar el nombre original del archivo
+    }
+});
+
+const upload = multer({ storage: storage }); // Crear una instancia de multer con la configuración de almacenamiento
+
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
