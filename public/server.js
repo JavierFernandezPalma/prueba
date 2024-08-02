76d3ba754524961cd8172cc2ba4a861b9e085749
jaaferna <jaaferna@bancolombia.com.co>
2024-08-02 14:59:56 -0500

    Ajuste código server

--- public/server.js
+++ public/server.js
@@ -1,6 +1,12 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express'); // Importar el módulo Express
+const multer = require('multer'); // Importar el módulo Multer para manejar la carga de archivos
+const cors = require('cors'); // Importar el módulo CORS para habilitar solicitudes desde otros dominios
+const fs = require('fs'); // Importar el módulo fs para manejar operaciones del sistema de archivos
+const path = require('path'); // Importar el módulo Path para trabajar con rutas de archivos y directorios
+const libxmljs = require('libxmljs2');
+
+const app = express(); // Crear una aplicación Express
+const PORT = 3000; // Definir el puerto en el que el servidor escuchará
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
