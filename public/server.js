221217b3c4e98504fcfdd49c78d002a15bf11679
jaaferna <jaaferna@bancolombia.com.co>
2024-08-02 14:57:04 -0500

    Ajuste código server

--- public/server.js
+++ public/server.js
@@ -1,18 +1,11 @@
-const express = require('express'); // Importar el módulo Express
-const multer = require('multer'); // Importar el módulo Multer para manejar la carga de archivos
-const cors = require('cors'); // Importar el módulo CORS para habilitar solicitudes desde otros dominios
-const fs = require('fs'); // Importar el módulo fs para manejar operaciones del sistema de archivos
-const path = require('path'); // Importar el módulo Path para trabajar con rutas de archivos y directorios
-const libxmljs = require('libxmljs2');
-
-const app = express(); // Crear una aplicación Express
-const PORT = process.env.PORT || 3000; // Definir el puerto en el que el servidor escuchará
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`El servidor está corriendo en http://localhost:${PORT}`); // Imprimir un mensaje cuando el servidor esté listo
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
