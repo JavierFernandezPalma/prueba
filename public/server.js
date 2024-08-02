c340e23a20f086c7057a573ee7a0a5eacccddbb9
jaaferna <jaaferna@bancolombia.com.co>
2024-08-02 15:10:35 -0500

    Ajuste código server

--- public/server.js
+++ public/server.js
@@ -78,6 +78,144 @@ app.post('/upload', upload.single('file'), (req, res) => {
 
 });
 
+// Ruta para obtener la lista de archivos XML
+app.get('/xmlfiles', (req, res) => {
+    const directoryPath = path.join(__dirname, 'upload'); // Definir la ruta del directorio de subida
+
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) { // Manejar errores al leer el directorio
+            console.error('No se puede escanear el directorio:', err);
+            return res.status(500).send('Error al obtener la lista de archivos.');
+        }
+
+        const xmlFiles = files.filter(file => file.endsWith('.xml')); // Filtrar solo archivos XML
+        res.status(200).json(xmlFiles); // Enviar la lista de archivos XML como respuesta
+    });
+});
+
+// Ruta para la eliminación de archivos
+app.delete('/delete', (req, res) => {
+    const fullPath = req.body.fileName; // Obtener la ruta completa del archivo
+    const fileName = path.basename(fullPath); // Obtener solo el nombre del archivo
+    const relativeUploadPath = path.join('upload', fileName); // Ruta correcta para el archivo a eliminar
+    const absoluteUploadPath = path.join(__dirname, relativeUploadPath); // Ruta absoluta del archivo a eliminar
+
+    const filePath = path.join(__dirname, 'data', 'files.json'); // Ruta al archivo files.json
+
+    // Verificar si el archivo existe antes de intentar eliminarlo
+    if (!fs.existsSync(absoluteUploadPath)) {
+        console.error('El archivo no existe en la ruta especificada:', absoluteUploadPath);
+        return res.status(404).send('El archivo no existe.');
+    }
+
+    // Eliminar el archivo
+    fs.unlink(absoluteUploadPath, (err) => {
+        if (err) {
+            console.error('Error al eliminar el archivo:', err);
+            return res.status(500).send('Error al eliminar el archivo.');
+        }
+
+        // Leer el archivo JSON existente
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error leyendo files.json:', err);
+                return res.status(500).send('Error al eliminar el registro del archivo.');
+            }
+
+            let files = { xmlFiles: [] };
+            if (data) {
+                try {
+                    files = JSON.parse(data); // Parsear el contenido JSON del archivo
+                } catch (jsonErr) {
+                    console.error('Error parseando JSON:', jsonErr);
+                    return res.status(500).send('Error al eliminar el registro del archivo.');
+                }
+            }
+
+            // Filtrar el array para eliminar el archivo
+            files.xmlFiles = files.xmlFiles.filter(file => file !== fileName);
+
+            // Escribir el array actualizado de vuelta al archivo JSON
+            fs.writeFile(filePath, JSON.stringify(files, null, 2), 'utf8', (writeErr) => {
+                if (writeErr) {
+                    console.error('Error escribiendo files.json:', writeErr);
+                    return res.status(500).send('Error al eliminar el registro del archivo.');
+                }
+                console.log('Archivo eliminado y registrado borrado exitosamente'); // Imprimir un mensaje de éxito en la consola
+                res.status(200).send('Archivo eliminado exitosamente.');
+            });
+        });
+    });
+});
+
+// Endpoint para la validación del XML
+app.post('/validate', (req, res) => {
+    const { xml, xsd } = req.body;
+
+    // console.log('XML:', xml);
+    // console.log('XSD:', xsd);
+
+    // Primero, verifica si el XSD es válido
+    let xsdDoc;
+    try {
+        xsdDoc = libxmljs.parseXml(xsd);
+        // console.log(xsdDoc);
+    } catch (err) {
+        // XSD inválido
+        console.error('Error al parsear el XSD:', err);
+        return res.status(400).json({ error: 'El esquema XSD proporcionado es inválido.', details: err.message });
+    }
+
+    try {
+
+        // Parse XML
+        const xmlDoc = libxmljs.parseXml(xml);
+        // Validar XML contra XSD
+        const isValid = xmlDoc.validate(xsdDoc);
+
+        if (isValid) {
+            console.log('Validación:', isValid);
+            res.status(200).json({ isValid, message: 'XML válido respecto al XSD.' });
+        } else {
+            console.log('Errores de validación:', xmlDoc.validationErrors);
+            const validationErrors = xmlDoc.validationErrors.map(err => ({
+                message: err.message,
+                line: err.line,
+                column: err.column
+            }));
+            res.status(400).json({
+                isValid: false,
+                message: 'El XML no es válido respecto al XSD.',
+                validationErrors: validationErrors
+            });
+        }
+    } catch (error) {
+        // console.error('Error al validar XML contra XSD:', error);
+        // Manejar errores relacionados con el esquema XSD
+        if (error.message.includes('Invalid XSD schema')) {
+            // Inicializar el array
+            let validationErrors = [];
+            // Agregar objetos al array
+            validationErrors.push({ message: error.message });
+            res.status(400).json({
+                isValid: false,
+                message: 'Error en el esquema XSD.',
+                validationErrors: validationErrors
+            });
+        } else {
+            // Manejar otros errores internos
+            // console.error('Error al validar XML contra XSD:', error);
+            res.status(500).json({
+                error: 'Error interno al validar XML contra XSD.',
+                details: {
+                    message: error.message,
+                    stack: error.stack
+                }
+            });
+        }
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
